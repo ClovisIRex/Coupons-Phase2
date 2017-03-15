44e117d8dde021d4c1abefb611373282e404f1cf
ClovisIRex <livny.tal@gmail.com>
2017-03-15 10:07:14 +0200

    removed angular for now

---
+++ WebContent/app.js
@@ -0,0 +1,19 @@
+var app = angular.module("SolInvictus", ["ngRoute"]);
+
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "login/login.htm"
+    })
+    .when("/login", {
+        templateUrl : "login/login.htm",
+        controller: "LoginController",
+        controllerAs: "vm"
+
+    })
+    .when("/register", {
+        templateUrl : "register/register.htm"
+    })
+    .otherwise({ redirectTo: '/login' });
+});
