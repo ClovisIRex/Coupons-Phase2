7a837109e3e4f752886047e32c89bb0a56f46112
ClovisIRex <livny.tal@gmail.com>
2017-03-16 13:11:24 +0200

    finished css for login and register

--- WebContent/app.js
+++ WebContent/app.js
@@ -3,17 +3,51 @@ var app = angular.module("SolInvictus", ["ngRoute"]);
 
 app.config(function($routeProvider) {
     $routeProvider
+    /*
+    ---------LOGIN + REGISTER VIEWS--------
+    */
     .when("/", {
         templateUrl : "login/login.htm"
     })
     .when("/login", {
-        templateUrl : "login/login.htm",
         controller: "LoginController",
+        templateUrl : "login/login.htm",
         controllerAs: "vm"
+    })
+    .when("/register.home", {
+        templateUrl : "register/register.home.htm"
+    })
+    .when('/register.customer', {
+			controller: 'RegisterCustomerController',
+			templateUrl: 'register/register.customer.htm',
+			controllerAs: 'vm'
+		})
 
-    })
-    .when("/register", {
-        templateUrl : "register/register.htm"
-    })
+		.when('/register.company', {
+			controller: 'RegisterCompanyController',
+			templateUrl: 'register/register.company.htm',
+			controllerAs: 'vm'
+		})
+
+    /*
+    ---------ADMIN VIEWS--------
+    */
+    .when('/admin.home', {
+			controller: 'AdminController',
+			templateUrl: 'admin/admin.home.view.htm',
+			controllerAs: 'vm'
+		})
+
+		.when('/admin.companies', {
+			controller: 'AdminCompaniesController',
+			templateUrl: 'admin/admin.companies.view.htm',
+			controllerAs: 'vm'
+		})
+
+		.when('/admin.customers', {
+			controller: 'AdminCustomersController',
+			templateUrl: 'admin/admin.customers.view.htm',
+			controllerAs: 'vm'
+		})
     .otherwise({ redirectTo: '/login' });
 });
